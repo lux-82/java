b0d0abb3ef3b38907c374345468f3eed1893c5fd
hikari <pierresuzuki82@gmail.com>
2022-03-22 20:59:11 +0900

    FizzBUzz

---
+++ FizzBuzz.java
@@ -0,0 +1,23 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            return;
+        }
+        int max = Integer.parseInt(args[0]);
+        String result = null;
+        for (int i = 1; i <= max; i++) {
+            result = "";
+            
+            if (i % 3 == 0) {
+                result = result + "Fizz";
+            }
+            if (i % 5 == 0) {
+                result = result + "Buzz";
+            }
+            if (result.length() == 0) {
+                result = result + i;
+            }
+            System.out.print(result + " ");
+        }
+    }
+}
